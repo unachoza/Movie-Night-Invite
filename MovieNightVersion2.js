bc65e5d9c9977c8ce53ac79c7ee7c47676f7ba9c
Arianna Choza <unachoza@gmail.com>
2019-01-17 17:07:37 -0800

    transfering movie night project

---
+++ MovieNightVersion2.js
@@ -0,0 +1,15 @@
+function main() {
+    var $intro = $('.intro');
+    $('.intro').hide();
+    $('.intro').fadeIn(900);
+    $('.skillset').hide();
+    $('.skillset').fadeIn(2000);
+    $('.projects').hide();
+    $('.projects-button').on('click', function() {
+        $(this).next().slideToggle(400);
+        $(this).toggleClass('active');
+        $(this).text('Did you write that down?');
+        });
+}
+  
+  $(document).ready(main);
